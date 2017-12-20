fd1dc4dec0292c0385a1b1748a8aa9f94ad8a3fc
bibek0914 <34020216+bibek0914@users.noreply.github.com>
2017-12-20 18:01:33 +0530

    Update ClassTest1.java

--- src/test/java/example1/ClassTest1.java
+++ src/test/java/example1/ClassTest1.java
@@ -12,7 +12,7 @@ public class ClassTest1 {
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\bibek\\Downloads\\chromedriver.exe");		
 	WebDriver driver=new ChromeDriver();
     driver.get("https://www.amazon.in/");
-	
+	//editing this
 	String atitle=driver.getTitle();
 	String etitle="Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
 	Assert.assertEquals(atitle, etitle);//AssertEquals will check exact content.
